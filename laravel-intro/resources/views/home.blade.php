c80bf7c27c78bf875812d0c2d601a08b883e2ae6
andyalam <andyalam@hotmail.com>
2018-01-11 19:30:52 -0800

    templating

---
+++ laravel-intro/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="center">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cupiditate dolores esse iusto maiores nesciunt saepe similique temporibus, vero. Accusamus architecto at consectetur cumque, eius omnis similique soluta temporibus voluptates.
+        </p>
+    </div>
+@endsection
