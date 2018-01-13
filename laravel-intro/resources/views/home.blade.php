eab9a400fe1c6fbedda64c7fb831612de0ded26c
andyalam <andyalam@hotmail.com>
2018-01-12 22:24:34 -0800

    templating

--- laravel-intro/resources/views/home.blade.php
+++ laravel-intro/resources/views/home.blade.php
@@ -5,5 +5,12 @@
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cupiditate dolores esse iusto maiores nesciunt saepe similique temporibus, vero. Accusamus architecto at consectetur cumque, eius omnis similique soluta temporibus voluptates.
         </p>
+        <ul>
+            @for($i = 0; $i < 5; $i++)
+                @if($i % 2 === 0)
+                    <li>Iteration {{ $i + 1 }}</li>
+                @endif
+            @endfor
+        </ul>
     </div>
 @endsection
