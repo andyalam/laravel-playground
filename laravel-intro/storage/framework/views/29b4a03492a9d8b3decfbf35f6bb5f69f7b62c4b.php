111342982c22ee38a57b807855b59ba0902122df
andyalam <andyalam@hotmail.com>
2018-01-22 18:33:11 -0800

    misc

--- laravel-intro/storage/framework/views/29b4a03492a9d8b3decfbf35f6bb5f69f7b62c4b.php
+++ laravel-intro/storage/framework/views/29b4a03492a9d8b3decfbf35f6bb5f69f7b62c4b.php
@@ -3,9 +3,6 @@
         <?php $__currentLoopData = $actions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $action): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <a href="<?php echo e(route('niceaction', ['action' => lcfirst($action->name)])); ?>"><?php echo e($action->name); ?></a>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        <a href="<?php echo e(route('niceaction', ['action' => 'greet'])); ?>">Greet</a>
-        <a href="<?php echo e(route('niceaction', ['action' => 'shake'])); ?>">Shake</a>
-        <a href="<?php echo e(route('niceaction', ['action' => 'hug'])); ?>">Hug</a>
         <br><br>
         <?php if(count($errors) > 0): ?>
             <div>
